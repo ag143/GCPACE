4dfe2dfa5c4cb7e8563cbf498f77de304f7fcedf
ag143 <ag14341@gmail.com>
2024-04-21 20:20:55 +0530

    Add files via upload

--- GCP_Menu/dist/script.js
+++ GCP_Menu/dist/script.js
@@ -740,10 +740,16 @@ $(document).on('ready', function() {
             });
             var subMenu = (exists) ? '<ul>' + getMenu(node.id).join('') + '</ul>' : "";
 			var returnValue = '';
-            if (node.depth === 0)
-                returnValue = '<li><a href="#">' + node.name + '<em class="mdi mdi-chevron-down"></em></a>' + subMenu;
-            else 
-				returnValue= '<li><a href="#">'+node.name + '</a></li>' +subMenu; 
+			console.log("node.name-->"+node.name+"-->nod.parentID-->"+node.parent_id+"-->node.depth-->"+node.depth);
+			console.log(node.depth);
+            if(node.parent_id == '0')
+			{
+                returnValue = '<li><a href=#'+node.name+' style="background-color: grey;"if(node.parent_id == 0)>' + node.name + '</a>' + subMenu;
+			}
+            if(node.parent_id !== 0)
+			{
+                returnValue = '<li><a href=#'+node.name+' style="background-color: Grey;">'+node.name + '</a></li>' +subMenu; 
+			}
             //console.log("return value-->"+returnValue);
             return returnValue;
         });
@@ -752,3 +758,80 @@ $(document).on('ready', function() {
     $('#menu').html('<ul>' + endMenu.join('') + '</ul>');
 
 })
+ $(function(){
+  var $ul   =   $('.sidebar-navigation > ul');
+  
+  $ul.find('li a').click(function(e){
+    var $li = $(this).parent();
+    
+    if($li.find('ul').length > 0){
+      e.preventDefault();
+      
+      if($li.hasClass('selected')){
+        $li.removeClass('selected').find('li').removeClass('selected');
+        $li.find('ul').slideUp(400);
+        $li.find('a em').removeClass('mdi-flip-v');
+      }else{
+        
+        if($li.parents('li.selected').length == 0){
+          $ul.find('li').removeClass('selected');
+          $ul.find('ul').slideUp(400);
+          $ul.find('li a em').removeClass('mdi-flip-v');
+        }else{
+          $li.parent().find('li').removeClass('selected');
+          $li.parent().find('> li ul').slideUp(400);
+          $li.parent().find('> li a em').removeClass('mdi-flip-v');
+        }
+        
+        $li.addClass('selected');
+        $li.find('>ul').slideDown(400);
+        $li.find('>a>em').addClass('mdi-flip-v');
+      }
+    }
+  });
+  
+  
+  $('.sidebar-navigation > ul ul').each(function(i){
+    if($(this).find('>li>ul').length > 0){
+      var paddingLeft = $(this).parent().parent().find('>li>a').css('padding-left');
+      var pIntPLeft   = parseInt(paddingLeft);
+      var result      = pIntPLeft + 20;
+      
+      $(this).find('>li>a').css('padding-left',result);
+    }else{
+      var paddingLeft = $(this).parent().parent().find('>li>a').css('padding-left');
+      var pIntPLeft   = parseInt(paddingLeft);
+      var result      = pIntPLeft + 20;
+      
+      $(this).find('>li>a').css('padding-left',result).parent().addClass('selected--last');
+    }
+  });
+  
+  var t = ' li > ul ';
+  for(var i=1;i<=10;i++){
+    $('.sidebar-navigation > ul > ' + t.repeat(i)).addClass('subMenuColor' + i);
+  }
+  
+  var activeLi = $('li.selected');
+  if(activeLi.length){
+    opener(activeLi);
+  }
+  
+  function opener(li){
+    var ul = li.closest('ul');
+    if(ul.length){
+      
+        li.addClass('selected');
+        ul.addClass('open');
+        li.find('>a>em').addClass('mdi-flip-v');
+      
+      if(ul.closest('li').length){
+        opener(ul.closest('li'));
+      }else{
+        return false;
+      }
+      
+    }
+  }
+  
+});
